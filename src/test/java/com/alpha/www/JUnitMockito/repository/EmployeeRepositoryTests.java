c3936b6773149085f9954b5d45efee43d40d2ac5
NM <masihnishit@gmail.com>
2022-11-01 17:17:08 +0530

    video 21

--- src/test/java/com/alpha/www/JUnitMockito/repository/EmployeeRepositoryTests.java
+++ src/test/java/com/alpha/www/JUnitMockito/repository/EmployeeRepositoryTests.java
@@ -195,4 +195,27 @@ public class EmployeeRepositoryTests {
         assertThat(savedEmployee).isNotNull();
     }
 
+    // JUnit test for custom query using Native SQL with index params
+    @DisplayName("JUnit test for custom query using Native SQL with index params")
+    @Test
+    public void givenFirstNameAndLastName_whenFindByNativeSQL_thenReturnEmployeeObject(){
+        // given - pre condition or setup
+        Employee employee=Employee.builder()
+                .firstName("tom")
+                .lastName("sharma")
+                .email("tom@gmail.com")
+                .build();
+
+        employeeRepository.save(employee);
+//        String firstName="tom";
+//        String lastName="sharma";
+
+        // when - action or behavior that we are going to test
+//        Employee savedEmployee=employeeRepository.findByNativeSQL(firstName,lastName);
+        Employee savedEmployee=employeeRepository.findByNativeSQL(employee.getFirstName(),employee.getLastName());
+
+        // then - verify the output
+        assertThat(savedEmployee).isNotNull();
+    }
+
 }
