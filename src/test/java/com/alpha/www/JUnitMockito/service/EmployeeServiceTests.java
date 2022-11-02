5490f336bb433e0574f96d4106ea03bf3326c05a
NM <masihnishit@gmail.com>
2022-11-03 00:58:38 +0530

    video 30

--- src/test/java/com/alpha/www/JUnitMockito/service/EmployeeServiceTests.java
+++ src/test/java/com/alpha/www/JUnitMockito/service/EmployeeServiceTests.java
@@ -1,6 +1,7 @@
 package com.alpha.www.JUnitMockito.service;
 
 import com.alpha.www.JUnitMockito.entity.Employee;
+import com.alpha.www.JUnitMockito.exception.ResourceNotFoundException;
 import com.alpha.www.JUnitMockito.repository.EmployeeRepository;
 import com.alpha.www.JUnitMockito.service.impl.EmployeeServiceImpl;
 import org.assertj.core.api.Assertions;
@@ -9,7 +10,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 //import org.mockito.BDDMockito;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -75,5 +80,26 @@ public class EmployeeServiceTests {
 
         // then - verify the output
         Assertions.assertThat(savedEmployee).isNotNull();
+    }
+
+    // JUnit test for saveEmployee method which throws exception
+    @DisplayName("JUnit test for saveEmployee method which throws exception")
+    @Test
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException(){
+
+        // given - pre condition or setup
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee));
+
+        System.out.println(employeeRepository);
+        System.out.println(employeeServiceImpl);
+
+        // when - action or behavior that we are going to test
+        org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, ()->{
+            employeeServiceImpl.saveEmployee(employee);
+        });
+
+        // then - verify the output
+        verify(employeeRepository, never()).save(any(Employee.class));
     }
 }
