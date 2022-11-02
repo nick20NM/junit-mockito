ae49e79728be92aaa1679fc32c7539f1297e24ab
NM <masihnishit@gmail.com>
2022-11-03 00:27:56 +0530

    video 29

--- src/test/java/com/alpha/www/JUnitMockito/service/EmployeeServiceTests.java
+++ src/test/java/com/alpha/www/JUnitMockito/service/EmployeeServiceTests.java
@@ -7,20 +7,38 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.BDDMockito;
+import static org.mockito.BDDMockito.given;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTests {
 
+    @Mock
     private EmployeeRepository employeeRepository;
-    private EmployeeService employeeService;
+
+    @InjectMocks
+    private EmployeeServiceImpl employeeServiceImpl;
+//    private EmployeeService employeeService;
+
+    private Employee employee;
 
     @BeforeEach
     public void setup(){
-        employeeRepository= Mockito.mock(EmployeeRepository.class);
-        employeeService=new EmployeeServiceImpl(employeeRepository);
+//        employeeRepository= Mockito.mock(EmployeeRepository.class);
+//        employeeService=new EmployeeServiceImpl(employeeRepository);
+        employee=Employee.builder()
+                .id(1L)
+                .firstName("tony")
+                .lastName("stark")
+                .email("tony@gmail.com")
+                .build();
     }
 
     // JUnit test for saveEmployee method
@@ -29,23 +47,29 @@ public class EmployeeServiceTests {
     public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject(){
 
         // given - pre condition or setup
-        Employee employee=Employee.builder()
-                .id(1L)
-                .firstName("tony")
-                .lastName("stark")
-                .email("tony@gmail.com")
-                .build();
+//        Employee employee=Employee.builder()
+//                .id(1L)
+//                .firstName("tony")
+//                .lastName("stark")
+//                .email("tony@gmail.com")
+//                .build();
 
-        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail()))
+//        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail()))
+//                .willReturn(Optional.empty());
+//        BDDMockito.given(employeeRepository.save(employee))
+//                .willReturn(employee);
+        given(employeeRepository.findByEmail(employee.getEmail()))
                 .willReturn(Optional.empty());
-        BDDMockito.given(employeeRepository.save(employee))
+        given(employeeRepository.save(employee))
                 .willReturn(employee);
 
         System.out.println(employeeRepository);
-        System.out.println(employeeService);
+//        System.out.println(employeeService);
+        System.out.println(employeeServiceImpl);
 
         // when - action or behavior that we are going to test
-        Employee savedEmployee=employeeService.saveEmployee(employee);
+//        Employee savedEmployee=employeeService.saveEmployee(employee);
+        Employee savedEmployee=employeeServiceImpl.saveEmployee(employee);
 
         System.out.println(savedEmployee);
 
