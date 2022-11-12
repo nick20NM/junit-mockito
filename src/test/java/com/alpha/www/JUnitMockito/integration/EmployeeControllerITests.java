4ecb1c712bab7b53dd01c39786abf7af6aed490d
NM <masihnishit@gmail.com>
2022-11-12 19:50:07 +0530

    video 57

---
+++ src/test/java/com/alpha/www/JUnitMockito/integration/EmployeeControllerITests.java
@@ -0,0 +1,28 @@
+package com.alpha.www.JUnitMockito.integration;
+
+import com.alpha.www.JUnitMockito.repository.EmployeeRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeControllerITests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup(){
+        employeeRepository.deleteAll();
+    }
+}
